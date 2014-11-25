3fe61a09721076ef18b56c575dba6ff3f91ca366
saeh <atkins.tommy@gmail.com>
2014-11-25 12:21:05 +1100

    load data into pandas
    Format scaped data into pandas dataframes, merge together, save to csv

--- pull_raw_data.py
+++ pull_raw_data.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import pandas
 
 ## Player Tracking Speed
 url = 'stats.nba.com/js/data/sportvu/speedData.js'
@@ -141,5 +142,32 @@ except:
     print 'Could not parse %s' % url
 
 
+#Load data into pandas dataframe
+speed_df = pandas.DataFrame(data=speed_values,columns=speed_headers[:7]+['SPEED_' + i for i in speed_headers[7:]])
+touches_df = pandas.DataFrame(data=touches_values,columns=touches_headers[:7]+['TOUCHES_' + i for i in touches_headers[7:]])
+passing_df = pandas.DataFrame(data=passing_values,columns=passing_headers[:7]+['PASSING_' + i for i in passing_headers[7:]])
+defense_df = pandas.DataFrame(data=defense_values,columns=defense_headers[:7]+['DEFENSE_' + i for i in defense_headers[7:]])
+rebounding_df = pandas.DataFrame(data=rebounding_values,columns=rebounding_headers[:7]+['REBOUNDING_' + i for i in rebounding_headers[7:]])
+drives_df = pandas.DataFrame(data=drives_values,columns=drives_headers[:7]+['DRIVES_' + i for i in drives_headers[7:]])
+catchshoot_df = pandas.DataFrame(data=catchshoot_values,columns=catchshoot_headers[:7]+['CATCHSHOOT_' + i for i in catchshoot_headers[7:]])
+pullup_df = pandas.DataFrame(data=pullup_values,columns=pullup_headers[:7]+['PULLUP_' + i for i in pullup_headers[7:]])
+shooting_df = pandas.DataFrame(data=shooting_values,columns=shooting_headers[:7]+['SHOOTING_' + i for i in shooting_headers[7:]])
+
+#shotchart data looks a little wrong.... don't merge in with rest
+shotchart_df = pandas.DataFrame(data=shotchart_values,columns=['SHOTCHART_' + i for i in shotchart_headers])
+
+#merge data frames together on common data points
+player_df = pandas.merge(speed_df,touches_df, on=['PLAYER_ID','PLAYER','FIRST_NAME','LAST_NAME','TEAM_ABBREVIATION','GP','MIN'], how='outer')
+player_df = pandas.merge(player_df,passing_df, on=['PLAYER_ID','PLAYER','FIRST_NAME','LAST_NAME','TEAM_ABBREVIATION','GP','MIN'], how='outer')
+player_df = pandas.merge(player_df,defense_df, on=['PLAYER_ID','PLAYER','FIRST_NAME','LAST_NAME','TEAM_ABBREVIATION','GP','MIN'], how='outer')
+player_df = pandas.merge(player_df,rebounding_df, on=['PLAYER_ID','PLAYER','FIRST_NAME','LAST_NAME','TEAM_ABBREVIATION','GP','MIN'], how='outer')
+player_df = pandas.merge(player_df,drives_df, on=['PLAYER_ID','PLAYER','FIRST_NAME','LAST_NAME','TEAM_ABBREVIATION','GP','MIN'], how='outer')
+player_df = pandas.merge(player_df,catchshoot_df, on=['PLAYER_ID','PLAYER','FIRST_NAME','LAST_NAME','TEAM_ABBREVIATION','GP','MIN'], how='outer')
+player_df = pandas.merge(player_df,pullup_df, on=['PLAYER_ID','PLAYER','FIRST_NAME','LAST_NAME','TEAM_ABBREVIATION','GP','MIN'], how='outer')
+player_df = pandas.merge(player_df,shooting_df, on=['PLAYER_ID','PLAYER','FIRST_NAME','LAST_NAME','TEAM_ABBREVIATION','GP','MIN'], how='outer')
+
+#save to csv
+player_df.to_csv('players.csv')
+shotchart_df.to_csv('shotchart.csv')
 
 
